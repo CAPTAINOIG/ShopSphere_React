b0d7f7e8a68e46e0be28388ac00234a8f851447f
abdullahi samsudeen <abdullahisamsudeen@gmail.com>
2024-09-07 19:33:59 +0100

    shopsphere

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,16 +1,18 @@
 // src/axiosConfig.js
 import axios from 'axios';
 
-const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000',
-});
-
-
 // const axiosInstance = axios.create({
-//     baseURL: 'http://localhost:3000',
-//     headers: {
-//         'Content-Type': 'application/json',
-//         Authorization: `Bearer ${localStorage.getItem('token')}`, // example of setting token
-//     }
+//     baseURL: 'https://shopsphere-node.onrender.com',
+//     // http://localhost:3000
+    
 // });
+
+
+const axiosInstance = axios.create({
+    baseURL: 'https://shopsphere-node.onrender.com',
+    headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${localStorage.getItem('token')}`, // example of setting token
+    }
+});
 export default axiosInstance;
