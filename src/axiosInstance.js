de5c2614ec71de4fb6616402ed1e43747e1110c3
abdullahi samsudeen <abdullahisamsudeen@gmail.com>
2024-09-17 14:09:27 +0100

    shoppinsphere

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -9,8 +9,8 @@ import axios from 'axios';
 
 
 const axiosInstance = axios.create({
-    // baseURL: 'https://shopsphere-node.onrender.com',
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://shopsphere-node.onrender.com',
+    // baseURL: 'http://localhost:3000',
     headers: {
         'Content-Type': 'application/json',
         Authorization: `Bearer ${localStorage.getItem('token')}`, // example of setting token
