25c4650a6e91fd92fa25c38f57a3483471a94ea6
abdullahi samsudeen <abdullahisamsudeen@gmail.com>
2025-01-08 13:21:36 +0100

    commit

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
     baseURL: 'https://shopsphere-node.onrender.com',
-    // baseURL: 'http://localhost:3000',
+    // baseURL: 'http://localhost:3000'
     headers: {
         'Content-Type': 'application/json',
         Authorization: `Bearer ${localStorage.getItem('token')}`,
